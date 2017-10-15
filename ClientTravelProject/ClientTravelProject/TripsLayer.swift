185de3ba9c637794871764865614b1188671df0e
matthewharrilal <matthewharrilal@gmail.com>
2017-10-14 17:31:47 -0700

    Added the modeling of both the users and their trips

--- ClientTravelProject/ClientTravelProject/TripsLayer.swift
+++ ClientTravelProject/ClientTravelProject/TripsLayer.swift
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 
 struct Trips {
-//    What we are essentially doing here is that we are saying that
+//    What we are essentially doing here is that we are saying that these are the properties that we want modeled for the data and since we have a non relational database we similarly enforcing a schema
     let email: String?
     let completed: Bool
     let destination:String?
@@ -28,5 +28,31 @@ struct Trips {
         self.latitude = latitude
         self.longitude = longitude
         self.waypointDestination = waypointDestination
+    }
+//    Here we are essentially initalizing a user therefore ever user will have these implementations 
+}
+
+extension Trips: Decodable {
+    enum additionalKeys: String, CodingKey {
+        case email
+        case completed
+        case destination
+        case startDate = "start_date"
+        case endDate = "end_date"
+        case latitude = "latitude"
+        case longitude = "longitude"
+        case waypointDestination = "waypoint_destination"
+    }
+     init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: additionalKeys.self)
+        let email = try container.decodeIfPresent(String.self, forKey: .email)
+        let completed = try container.decodeIfPresent(Bool.self, forKey: .completed)
+        let destination = try container.decodeIfPresent(String.self, forKey: .destination)
+        let startDate = try container.decodeIfPresent(String.self, forKey: .startDate)
+        let endDate = try container.decodeIfPresent(String.self, forKey: .endDate)
+        let latitude = try container.decodeIfPresent(String.self, forKey: .latitude)
+        let longitude = try container.decodeIfPresent(String.self, forKey: .longitude)
+        let waypointDestination = try container.decodeIfPresent(String.self, forKey: .waypointDestination)
+        self.init(email: email, completed: completed, destination: destination, startDate: startDate, endDate: endDate, latitude: latitude, longitude: longitude, waypointDestination: waypointDestination)
     }
 }
